83fa893d4889c91d8888418895548efab125009a
Dexter <96929816+thedexter46@users.noreply.github.com>
2022-08-14 21:11:56 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -4,4 +4,4 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.private & filters.command(["about"]))
 async def start(client,message):
-	await message.reply_text("Bot :- @rename_urbot\nCreater :- @mrlokaman\nLanguage :-Python3\nLibrary :- Pyrogram 1.4.16\nServer :- Heroku")
+	await message.reply_text("Bot :- @MC_Renamerbot\nCreater :-@dexte_r_46\nLanguage :-Python3\nLibrary :- Pyrogram 1.4.16\nServer :- Heroku")
